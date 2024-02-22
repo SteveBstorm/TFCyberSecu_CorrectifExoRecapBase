9df43804bb965e98fc4e1725a2bfe7420d7af732
stevebstorm <steve.lorent@gmail.Com>
2024-02-22 16:17:51 +0100

    Ajoutez des fichiers projet.

---
+++ CorrectifExoRecapBase/Magasin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectifExoRecapBase
+{
+    public static class Magasin
+    {
+        static Dictionary<string, int> ShopStore;
+        static Dictionary<string, int> ShoppingList;
+
+        public static void StoreInitialize()
+        {
+            ShopStore = new Dictionary<string, int>();
+            ShopStore.Add("Coca", 4);
+            ShopStore.Add("Sandwich", 2);
+            ShopStore.Add("Porte", 3);
+            ShopStore.Add("Boule de cristal", 7);
+
+            ShoppingList = new Dictionary<string, int>();
+        }
+
+        public static void Purchase(string article)
+        {
+            //Vérification du stock actuel
+            if (ShopStore[article] > 0)
+            {
+                ShopStore[article] -= 1;
+                //Si l'élément se trouve déjà dans mon panier
+                if(ShoppingList.ContainsKey(article))
+                {
+                    //J'incrémente le nombre s'y trouvant
+                    ShoppingList[article]++;
+                }
+                else
+                {
+                    //Sinon je le crée dans mon panier
+                    ShoppingList.Add(article, 1);
+                }
+            } else Console.WriteLine($"{article} n'est plus en stock");
+        }
+
+        public static int GetTotal()
+        {
+            int total = 0;
+            foreach (KeyValuePair<string, int> item in ShoppingList)
+            {
+                if (item.Key == "Coca") total += item.Value * 1;
+                if (item.Key == "Sandwich") total += item.Value * 5;
+                if (item.Key == "Porte") total += item.Value * 200;
+                if (item.Key == "Boule de cristal") 
+                {
+                    if (item.Value == 7) Console.WriteLine("Pour krilin on peut plus rien faire");
+                    total += item.Value * 2000;
+                        }
+                
+            }
+            return total;
+        }
+
+        public static int ShowItems()
+        {
+            Console.WriteLine("Faites votre choix : ");
+            Console.WriteLine();
+
+            int counter = 1;
+            foreach(KeyValuePair<string, int> item in ShopStore)
+            {
+                Console.WriteLine($"{counter} : {item.Key} ({item.Value} en stock)");
+                counter++;
+            }
+
+            Console.WriteLine($"{counter} pour Quitter");
+
+            int choix = int.Parse(Console.ReadLine());
+
+            return choix;
+        }
+    }
+}
---
+++ CorrectifExoRecapBase/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CorrectifExoRecapBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, Bienvenue chez Yvan du reve");
+            Console.WriteLine("------------------------------------");
+
+            Magasin.StoreInitialize();
+
+            bool continueShopping = true;
+            while (continueShopping)
+            {
+                int choix = Magasin.ShowItems();
+
+                switch (choix)
+                {
+                    case 1: Magasin.Purchase("Coca");
+                        break;
+                    case 2: Magasin.Purchase("Sandwich");
+                        break;
+                    case 3: Magasin.Purchase("Porte");
+                        break;
+                    case 4:
+                        Magasin.Purchase("Boule de cristal");
+                        break;
+                    case 5: continueShopping = false;
+                        break;
+                    default:
+                        Console.WriteLine("Tu sais pas lire un menu ?");
+                        break;
+
+                }
+            }
+
+            Console.WriteLine($"Le total de vos achats est de {Magasin.GetTotal()}");
+            Console.WriteLine("Merci de votre visite ! A ciao bon dimanche");
+        }
+    }
+}
